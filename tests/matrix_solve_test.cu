955d41cc182fbbcf6eec6fe247b5115d74a51f97
anthony-yip <anthony.ch.yip@gmail.com>
2024-07-01 13:57:56 +0800

    replace printing for cuMatSol test with assert

--- tests/matrix_solve_test.cu
+++ tests/matrix_solve_test.cu
@@ -5,6 +5,8 @@
 #include "../src/matrix_solve.h"
 #include <cassert>
 #include "../src/matsol_thrust.cuh"
+#include <thrust/for_each.h>
+
 void test_3x3() {
     MatSol<3>::Matrix matrix; MatSol<3>::Vector vector; MatSol<3>::Vector correct_solution;
     MatSol<3>::Vector solution;
@@ -23,6 +25,15 @@ void test_3x3() {
     assert(MatSol<3>::VectorEquals(solution, correct_solution));
 }
 
+namespace
+{
+    struct Round : public thrust::unary_function<float, void>
+    {
+        __device__ void operator()(float& x) const {
+            x = (float) round(x);
+        }
+    };
+}
 void test_3x3_cuSolver() {
     thrust::device_vector<float> matrix(9);
     thrust::device_vector<float> vector(3);
@@ -33,15 +44,13 @@ void test_3x3_cuSolver() {
     vector = {7, 16, 10};
     correct_solution = {1, 2, 3};
     cuMatSol<3>::solve(matrix, vector, solution);
-    thrust::copy(solution.begin(), solution.end(), std::ostream_iterator<float>(std::cout, " "));
-    // std::for_each(solution.begin(), solution.end(), [](float& x) { x = round(x); });
-    // assert(thrust::equal(solution.begin(), solution.end(), correct_solution.begin()));
+    thrust::for_each(solution.begin(), solution.end(), Round {});
+    assert(thrust::equal(solution.begin(), solution.end(), correct_solution.begin()));
 
     matrix = {1, 2, 3, 2, 5, 3, 1, 0, 8};
     vector = {13, 18, 26};
     correct_solution = {2,1,3};
     cuMatSol<3>::solve(matrix, vector, solution);
-    thrust::copy(solution.begin(), solution.end(), std::ostream_iterator<float>(std::cout, " "));
-    // std::for_each(solution.begin(), solution.end(), [](float& x) { x = round(x); });
-    // assert(thrust::equal(solution.begin(), solution.end(), correct_solution.begin()));
+    thrust::for_each(solution.begin(), solution.end(), Round {});
+    assert(thrust::equal(solution.begin(), solution.end(), correct_solution.begin()));
 }
